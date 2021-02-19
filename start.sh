3ebb658f9aec1eaf90d80c53a659ab23bd6c4c95
www <1249055292@qq.com>
2021-02-20 04:52:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,8 +34,10 @@ initNode() {
   sleep 3
   #写入地址
   ethAddress=$(cat $logFile | grep "using ethereum address" | awk '{print $6}')
+  echo $ethAddress
   echo "$ethAddress" >>ethAddressFile/node1
   peerAddress=$(cat node1.file | grep "using swarm network address through clef" | awk '{print $9}')
+  echo peerAddress
   echo "$peerAddress" >>peerAddressFile/node1
 }
 
