126da4cc00c3204e7def652280bbe056297adbeb
www <1249055292@qq.com>
2021-02-20 03:38:05 +0800

    Update initServer.sh

--- initServer.sh file:/Users/linhuiliu/Desktop/code/bbq/bbq/bbq-admin/src/main/java/org/npi/admin/modules/business/swarm/initServer.sh
+++ initServer.sh file:/Users/linhuiliu/Desktop/code/bbq/bbq/bbq-admin/src/main/java/org/npi/admin/modules/business/swarm/initServer.sh
@@ -1,92 +1,41 @@
-#1/usr/bin/env sh
-DEBUG_API=http://localhost:1635
-MIN_AMOUNT=1000
+#!/usr/bin/env sh
+beePath='/opt/bee'
+passFile='/opt/bee/beePass.txt'
 
-function getPeers() {
-  curl -s "$DEBUG_API/chequebook/cheque" | jq -r '.lastcheques | .[].peer'
+update() {
+  echo '开始更新'
+  apt update
+  apt upgrade -y
+  apt autoremove -y
+  apt install openssl jq -y
+  echo '更新结束'
 }
 
-function getCumulativePayout() {
-  local peer=$1
-  local cumulativePayout=$(curl -s "$DEBUG_API/chequebook/cheque/$peer" | jq '.lastreceived.payout')
-  if [ $cumulativePayout == null ]
-  then
-    echo 0
-  else
-    echo $cumulativePayout
-  fi
+installBeeClef() {
+  wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.7/bee-clef_0.4.7_amd64.deb
+  sudo dpkg -i bee-clef_0.4.7_amd64.deb
+  service bee-clef restart
 }
 
-function getLastCashedPayout() {
-  local peer=$1
-  local cashout=$(curl -s "$DEBUG_API/chequebook/cashout/$peer" | jq '.cumulativePayout')
-  if [ $cashout == null ]
-  then
-    echo 0
-  else
-    echo $cashout
-  fi
+installBeeClient() {
+  wget https://github.com/ethersphere/bee/releases/download/v0.5.0/bee_0.5.0_amd64.deb
+  sudo dpkg -i bee_0.5.0_amd64.deb
 }
 
-function getUncashedAmount() {
-  local peer=$1
-  local cumulativePayout=$(getCumulativePayout $peer)
-  if [ $cumulativePayout == 0 ]
-  then
-    echo 0
-    return
-  fi
-
-  cashedPayout=$(getLastCashedPayout $peer)
-  let uncashedAmount=$cumulativePayout-$cashedPayout
-  echo $uncashedAmount
+initPass() {
+  mkdir -p /opt/bee
+  random=$(openssl rand -base64 24)
+  echo "$random" >>$passFile
 }
 
-function cashout() {
-  local peer=$1
-  txHash=$(curl -s -XPOST "$DEBUG_API/chequebook/cashout/$peer" | jq -r .transactionHash)
-
-  echo cashing out cheque for $peer in transaction $txHash >&2
-
-  result="$(curl -s $DEBUG_API/chequebook/cashout/$peer | jq .result)"
-  while [ "$result" == "null" ]
-  do
-    sleep 5
-    result=$(curl -s $DEBUG_API/chequebook/cashout/$peer | jq .result)
-  done
+initCashOutSh() {
+  cd $beePath
+  wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/7ba05095e0836735f4a648aefe52c584e18e065f/cashout.sh
+  chmod +x cashout.sh
 }
 
-function cashoutAll() {
-  local minAmount=$1
-  for peer in $(getPeers)
-  do
-    local uncashedAmount=$(getUncashedAmount $peer)
-    if (( "$uncashedAmount" > $minAmount ))
-    then
-      echo "uncashed cheque for $peer ($uncashedAmount uncashed)" >&2
-      cashout $peer
-    fi
-  done
-}
-
-function listAllUncashed() {
-  for peer in $(getPeers)
-  do
-    local uncashedAmount=$(getUncashedAmount $peer)
-    if (( "$uncashedAmount" > 0 ))
-    then
-      echo $peer $uncashedAmount
-    fi
-  done
-}
-
-case $1 in
-cashout)
-  cashout $2
-  ;;
-cashout-all)
-  cashoutAll $MIN_AMOUNT
-  ;;
-list-uncashed|*)
-  listAllUncashed
-  ;;
+update
+installBeeClef
+installBeeClient
+initPass
+initCashOutSh
