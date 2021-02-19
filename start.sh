548844d848ea7259836a4af172453973a8d0d0d2
www <1249055292@qq.com>
2021-02-20 04:16:24 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,13 +11,13 @@ fi
 initNode() {
 nohup bee start \
   --verbosity 5 \
-  --debug-api-addr 1635 \
-  --p2p-addr 1634 \
   --api-addr 1633 \
+   --p2p-addr 1634 \
+   --debug-api-addr 1635 \
   --data-dir ${dataBasePath}'/' \
   --password-file $passFile \
   --cors-allowed-origins "*" \
-  --swap-endpoint https://rpc.slock.it/goerli \
+  --swap-endpoint https://goerli.infura.io/v3/40ace318b48b4a7da4694e5e4863f8d0 \
   --debug-api-enable \
   --clef-signer-enable \
   --clef-signer-endpoint /var/lib/bee-clef/clef.ipc \
