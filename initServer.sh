94be8b49831215fecd4ba5b574bd0f0b876b9291
linhuiliu <1249055292@qq.com>
2021-02-24 15:17:15 +0800

    a

--- initServer.sh
+++ initServer.sh
@@ -11,14 +11,14 @@ update() {
 }
 
 installBeeClef() {
-  wget https://bbq-chhain.oss-cn-shanghai.aliyuncs.com/files/20191128/bee-clef_0.4.7_amd64.deb
+  wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.7/bee-clef_0.4.7_amd64.deb
   dpkg -i bee-clef_0.4.7_amd64.deb
   service bee-clef restart
   rm -rf bee-clef_0.4.7_amd64.deb*
 }
 
 installBeeClient() {
-  wget https://bbq-chhain.oss-cn-shanghai.aliyuncs.com/files/20191128/bee_0.5.0_amd64.deb
+  wget https://github.com/ethersphere/bee/releases/download/v0.5.0/bee_0.5.0_amd64.deb
   dpkg -i bee_0.5.0_amd64.deb
   rm -rf bee_0.5.0_amd64.deb*
 }
