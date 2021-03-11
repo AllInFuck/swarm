421113f57c82dcdfa5fdcc491c9f907575c037b7
liulinhui <1249055292@qq.com>
2021-03-11 18:29:25 +0800

    no clef

---
+++ restart-noclef.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env sh
+dataBasePath='/opt/beeData'
+logBasePath='/opt/beeLogs'
+capacity="20000000"
+
+#api-addr 1633
+if [ x"$1" = x ]; then
+  exit 0
+fi
+#p2p-addr 1634
+if [ x"$2" = x ]; then
+  exit 0
+fi
+#debug-api-addr 1635
+if [ x"$3" = x ]; then
+  exit 0
+fi
+#name node1
+if [ x"$4" = x ]; then
+  exit 0
+fi
+
+#--db-capacity
+if [ x"$5" = x ]; then
+  exit 0
+fi
+
+# --swap-endpoint
+if [ x"$6" = x ]; then
+  exit 0
+fi
+
+api_addr=$1
+p2p_addr=$2
+debug_addr=$3
+nodeName=$4
+capacity=$5
+#--swap-endpoint https://goerli.infura.io/v3/40ace318b48b4a7da4694e5e4863f8d0 \
+swapEndpoint=$6
+
+passFile='/opt/bee/'${nodeName}.pass
+
+killOld() {
+ sudo kill -9 $(pidof bee)
+}
+
+initNode() {
+  logFile=${logBasePath}/$nodeName
+  nohup bee start \
+    --verbosity 3 \
+    --api-addr :${api_addr} \
+    --p2p-addr :${p2p_addr} \
+    --debug-api-addr :${debug_addr} \
+    --data-dir ${dataBasePath}/$nodeName \
+    --password-file $passFile \
+    --db-capacity $capacity \
+    --swap-endpoint $swapEndpoint \
+    --debug-api-enable \
+    >$logFile 2>&1 &
+}
+
+service bee-clef restart
+#杀掉之前的进程
+killOld
+initNode
---
+++ start-noclef.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env sh
+dataBasePath='/opt/beeData'
+logBasePath='/opt/beeLogs'
+ethAddressFile='/opt/bee/ethAddress'
+peerAddressFile='/opt/bee/peerAddress'
+randomPass=''
+capacity="20000000"
+mkdir -p $logBasePath
+mkdir -p $ethAddressFile
+mkdir -p $peerAddressFile
+
+#api-addr 1633
+if [ x"$1" = x ]; then
+  exit 0
+fi
+#p2p-addr 1634
+if [ x"$2" = x ]; then
+  exit 0
+fi
+#debug-api-addr 1635
+if [ x"$3" = x ]; then
+  exit 0
+fi
+#name node1
+if [ x"$4" = x ]; then
+  exit 0
+fi
+
+#--db-capacity
+if [ x"$5" != x ]; then
+  capacity=$5
+fi
+# --swap-endpoint
+if [ x"$6" = x ]; then
+  exit 0
+fi
+api_addr=$1
+p2p_addr=$2
+debug_addr=$3
+nodeName=$4
+#--swap-endpoint https://goerli.infura.io/v3/40ace318b48b4a7da4694e5e4863f8d0 \
+swapEndpoint=$6
+
+passFile='/opt/bee/'${nodeName}.pass
+
+initPass() {
+  randomPass=$(openssl rand -base64 24)
+  echo "$randomPass" >$passFile
+}
+
+initNode() {
+  logFile=${logBasePath}/$nodeName
+  nohup bee start \
+    --verbosity 3 \
+    --api-addr :${api_addr} \
+    --p2p-addr :${p2p_addr} \
+    --debug-api-addr :${debug_addr} \
+    --data-dir ${dataBasePath}/$nodeName \
+    --password-file $passFile \
+    --db-capacity $capacity \
+    --swap-endpoint $swapEndpoint \
+    --debug-api-enable \
+    >$logFile 2>&1 &
+
+  sleep 10
+  #写入地址
+  ethAddress=$(cat $logFile | grep "using ethereum address" | awk '{print $6}')
+  echo '以太坊地址：'ethetheth0x${ethAddress}ethetheth
+  echo "$ethAddress" >$ethAddressFile/$nodeName
+  peerAddress=$(cat $logFile | grep "using swarm network address through clef" | awk '{print $9}')
+  echo '节点地址：'peerpeerpeer${peerAddress}peerpeerpeer
+  echo "$peerAddress" >$peerAddressFile/$nodeName
+  #密码
+  echo '节点密码：'passpasspass${randomPass}passpasspass
+}
+
+#杀掉之前的进程
+#ps -ef | grep ${nodeName} | grep -v grep | awk '{print $2}' | xargs kill -9
+initPass
+initNode
