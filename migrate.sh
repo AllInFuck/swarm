a8d996dfab112ced0f9a3d9a1e07ecf2fd77a0f2
linhuiliu <1249055292@qq.com>
2021-03-14 06:28:34 +0800

    迁移

---
+++ migrate.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+beePath='/opt/bee'
+backUrl=$1
+
+mkdir -p $beePath
+
+update() {
+  echo '开始更新'
+  apt update
+  apt install openssl jq git curl wget -y
+  echo '更新结束'
+}
+
+installBeeClef() {
+  wget https://bbq-chhain.oss-cn-shanghai.aliyuncs.com/files/20191128/bee-clef_0.4.9_amd64.deb
+  dpkg -i bee-clef_0.4.9_amd64.deb
+  service bee-clef restart
+  rm -rf bee-clef_0.4.9_amd64.deb*
+}
+
+installBeeClient() {
+  wget https://bbq-chhain.oss-cn-shanghai.aliyuncs.com/files/20191128/bee_0.5.0_amd64.deb
+  dpkg -i bee_0.5.0_amd64.deb
+  rm -rf bee_0.5.0_amd64.deb*
+}
+
+initCashOutSh() {
+  # shellcheck disable=SC2164
+  cd $beePath
+  wget -O cashout.sh https://gitee.com/liulinhui1994/swarm/raw/main/cashout.sh
+  chmod +x cashout.sh
+}
+
+getBackup(){
+  cd /opt
+  mkdir migrate
+  cd migrate
+  wget -O swarm.tar.gz $backUrl
+  tar -zxvf swarm.tar.gz
+}
+
+update
+installBeeClef
+installBeeClient
+initCashOutSh
+getBackup
